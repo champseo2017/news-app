8acf10a8b8a97e4038f9c6363ced3a4fc81d368b
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-19 22:31:52 +0700

    16: 51
    https://www.youtube.com/watch?v=oIQgZ-WBluY&list=PLAP1EsuzlGNJYvsJzRE8owOzWEhIuZe3i&index=4

--- NewsAppProject/src/screens/authScreens/index.js
+++ NewsAppProject/src/screens/authScreens/index.js
@@ -1,5 +1,5 @@
-import React, {memo, useCallback} from 'react';
-import {Icon} from 'react-native-elements';
+import React, {memo, useCallback, useState, useRef} from 'react';
+import {Icon, Button} from 'react-native-elements';
 import {Header} from '../../components';
 import {styleSignIn} from './styled';
 import {View, Text, StyleSheet, Dimensions, TextInput} from 'react-native';
@@ -7,6 +7,9 @@ import {colors, parameters, container} from '../../global/styles';
 import * as Animatable from 'react-native-animatable';
 
 const SignInScreen = memo(() => {
+  const [textInput2Focused, setTextInput2Focused] = useState(false);
+  const textInput1 = useRef(1);
+  const textInput2 = useRef(2);
   const renderSignIn = useCallback(() => {
     return (
       <View style={container.container.container}>
@@ -27,10 +30,13 @@ const SignInScreen = memo(() => {
             <TextInput
               style={styleSignIn.textInput1.input}
               placeholder="Email"
+              ref={textInput1}
             />
           </View>
           <View style={styleSignIn.textInput2.input}>
-            <Animatable.View>
+            <Animatable.View
+              animation={textInput2Focused ? 'fadeInLeft' : ''}
+              duration={400}>
               <Icon
                 name="lock"
                 iconStyle={styleSignIn.textInput2.iconLock}
@@ -40,8 +46,17 @@ const SignInScreen = memo(() => {
             <TextInput
               placeholder="Password"
               style={styleSignIn.textInput2.inputPassword}
+              ref={textInput2}
+              onFocus={() => {
+                setTextInput2Focused(true);
+              }}
+              onBlur={() => {
+                setTextInput2Focused(false);
+              }}
             />
-            <Animatable.View>
+            <Animatable.View
+              animation={textInput2Focused ? 'fadeInLeft' : ''}
+              duration={400}>
               <Icon
                 name="visibility-off"
                 iconStyle={styleSignIn.textInput2.iconVisibilityOff}
@@ -50,9 +65,25 @@ const SignInScreen = memo(() => {
             </Animatable.View>
           </View>
         </View>
+        {/* Button */}
+        <View style={styleSignIn.buttonSignIn.container}>
+          <Button
+            title="SIGN IN"
+            buttonStyle={styleSignIn.buttonSignIn.button}
+            titleStyle={styleSignIn.buttonSignIn.title}
+          />
+        </View>
+        {/* Forgot Password */}
+        <View style={styleSignIn.forgotPassword.container}>
+          <Text style={styleSignIn.forgotPassword.title}>Forgot Password</Text>
+        </View>
+        {/* OR */}
+        <View style={styleSignIn.OR.container}>
+          <Text style={styleSignIn.OR.title}>OR</Text>
+        </View>
       </View>
     );
-  });
+  }, [textInput2Focused]);
   return renderSignIn();
 });
 
--- NewsAppProject/src/screens/authScreens/styled.js
+++ NewsAppProject/src/screens/authScreens/styled.js
@@ -51,14 +51,62 @@ export const styleSignIn = {
       paddingLeft: 15,
     },
     inputPassword: {
-      width: "80%"
+      width: '80%',
     },
     iconLock: {
-      color: colors.grey3
+      color: colors.grey3,
     },
     iconVisibilityOff: {
       color: colors.grey3,
-      marginRight: 10
-    }
+      marginRight: 10,
+    },
   },
+  buttonSignIn: {
+    container: {
+      marginHorizontal: 20,
+      marginTop: 30,
+    },
+    button: {
+      backgroundColor: '#ff8c52',
+      alignContent: 'center',
+      justifyContent: 'center',
+      borderRadius: 12,
+      borderWidth: 1,
+      borderColor: '#ff8c52',
+      height: 50,
+      paddingHorizontal: 20,
+      width: '100%',
+    },
+    title: {
+      color: 'white',
+      fontSize: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: -3,
+      fontFamily: Fonts.FontsBold,
+    },
+  },
+  forgotPassword: {
+    container: {
+      alignItems: 'center',
+      marginTop: 15,
+    },
+    title: {
+      color: colors.grey3,
+      fontSize: 16,
+      textDecorationLine: 'underline',
+      fontFamily: Fonts.FontsRegular,
+    },
+  },
+  OR: {
+    container: {
+      alignItems: 'center',
+      marginTop: 10,
+      marginBottom: 20,
+    },
+    title: {
+      fontSize: 20,
+      fontFamily: Fonts.FontsBold,
+    }
+  }
 };
